24a8d5cbbdbdf115310ac02b31502a3e2e2b1a7b
georgortwein <georgortwein@192.168.2.41>
2011-01-12 20:21:17 +0100

    thread test

---
+++ src/Data.java
@@ -0,0 +1,79 @@
+
+public class Data {
+	double oben;
+	double unten;
+	double front;
+	double frontUnten;
+	double frontOben;
+	
+	/**
+	 * @return the oben
+	 */
+	public double getOben() {
+		return oben;
+	}
+
+	/**
+	 * @param oben
+	 *            the oben to set
+	 */
+	public void setOben(double oben) {
+		this.oben = oben;
+	}
+
+	/**
+	 * @return the unten
+	 */
+	public double getUnten() {
+		return unten;
+	}
+
+	/**
+	 * @param unten the unten to set
+	 */
+	public void setUnten(double unten) {
+		this.unten = unten;
+	}
+
+	/**
+	 * @return the front
+	 */
+	public double getFront() {
+		return front;
+	}
+
+	/**
+	 * @param front the front to set
+	 */
+	public void setFront(double front) {
+		this.front = front;
+	}
+
+	/**
+	 * @return the frontUnten
+	 */
+	public double getFrontUnten() {
+		return frontUnten;
+	}
+
+	/**
+	 * @param frontUnten the frontUnten to set
+	 */
+	public void setFrontUnten(double frontUnten) {
+		this.frontUnten = frontUnten;
+	}
+
+	/**
+	 * @return the frontOben
+	 */
+	public double getFrontOben() {
+		return frontOben;
+	}
+
+	/**
+	 * @param frontOben the frontOben to set
+	 */
+	public void setFrontOben(double frontOben) {
+		this.frontOben = frontOben;
+	}
+}
---
+++ src/FisThread.java
@@ -0,0 +1,55 @@
+import org.jfree.chart.JFreeChart;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+
+
+public class FisThread  extends Thread {
+	FIS fis;
+	Thread thread;
+	Data data;
+	private JFreeChart test;
+	
+	public FisThread(Data data){
+		
+		super();
+		
+		this.data = data;
+		String fileName = "fcl.fcl";
+		fis = FIS.load(fileName, true);
+		fis.toStringFcl();
+
+		if (fis == null) { // Error while loading?
+			System.err.println("Can't load file: '" + fileName + "'");
+			return;
+		}
+		fis.chart();
+		fis.setVariable("oben", 100);
+		fis.setVariable("frontOben", 100);
+		fis.setVariable("front", 100);
+		fis.setVariable("frontUnten", 100);
+		fis.setVariable("unten", 100);
+		// fis.getFunctionBlock("helicopter").getVariable("oben").chart(true);
+		test = fis.getVariable("auftrieb").chartDefuzzifier(true);
+		start();
+	}
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+		System.out.println("DATA oben: "+data.getOben());
+		fis.setVariable("oben", data.getOben());
+		System.out.println("DATA unten: "+data.getUnten());
+		
+		fis.evaluate();
+		test.fireChartChanged();
+		//fis.getVariable("auftrieb").chartDefuzzifier(true);
+}
+		//fis.evaluate();
+	//	fis.getVariable("auftrieb").chartDefuzzifier(true);
+		
+	}
+	
+	public synchronized void setVariable(String name, double wert){
+		fis.setVariable(name, wert);
+	}
+
+}
